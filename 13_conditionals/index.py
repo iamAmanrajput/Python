2af84d3e4edfd0e8a99e6a06b5d8bfb808b9c3ac
Aman Kumar <aman.it360@gmail.com>
2025-09-01 07:18:45 +0530

    feat: Basics of conditionals in python

---
+++ 13_conditionals/index.py
@@ -0,0 +1,18 @@
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Number is Positive")
+elif num < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
+
+# Enter a number: 5
+# Number is Positive
+
+# Enter a number: -3
+# Number is Negative
+
+# Enter a number: 0
+# Number is Zero
