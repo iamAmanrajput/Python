4ea977df724f84f9e685d63ca3c564dedc265b39
Aman Kumar <aman.it360@gmail.com>
2025-09-02 08:18:59 +0530

    feat: add example of file open using loops

---
+++ 15_loops/file/index.py
@@ -0,0 +1,13 @@
+
+for line in open("file.txt"):
+    print(line)
+
+# Output
+
+# Hello, this is the first line.
+
+# This is the second line.
+
+# And here is the third line.
+
+# Python file handling is easy!
