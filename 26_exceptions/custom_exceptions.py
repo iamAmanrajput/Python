50833b8f312ce3e5452f10863dda089ce4bcb4ab
Aman Kumar <aman.it360@gmail.com>
2025-11-07 08:03:23 +0530

    feat: iadd custom exceptions example

---
+++ 26_exceptions/custom_exceptions.py
@@ -0,0 +1,18 @@
+def brew_chai(flavor):
+    if flavor not in ["masala", "ginger", "elaichai"]:
+        raise ValueError("Unsupported chai flavor...")
+    print(f"brewing {flavor} chai...")
+
+
+brew_chai("mint")
+
+# output
+
+#  python -u "c:\AMAN\Python\26_exceptions\custom_exceptions.py"
+# Traceback (most recent call last):
+#   File "c:\AMAN\Python\26_exceptions\custom_exceptions.py", line 7, in <module>
+#     brew_chai("mint")
+#     ~~~~~~~~~^^^^^^^^
+#   File "c:\AMAN\Python\26_exceptions\custom_exceptions.py", line 3, in brew_chai
+#     raise ValueError("Unsupported chai flavor...")
+# ValueError: Unsupported chai flavor...
